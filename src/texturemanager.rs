f8120e457c5e11797291cdc95d59f78e31871d6b
Matt Brubeck <mbrubeck@limpet.net>
2017-06-15 11:03:09 -0700

    Make TextureManager borrow TextureCreator

--- src/texturemanager.rs
+++ src/texturemanager.rs
@@ -7,19 +7,19 @@ use sdl2::image::{LoadTexture, INIT_PNG, INIT_JPG};
 use texture::Texture;
 
 pub struct TextureManager<'tm> {
-    texture_creator: TextureCreator<WindowContext>,
+    texture_creator: &'tm TextureCreator<WindowContext>,
     items: HashMap<String, Rc<Texture<'tm>>>,
 }
 
 impl<'tm> TextureManager<'tm> {
-    pub fn new(texture_creator: TextureCreator<WindowContext>) -> TextureManager<'tm> {
+    pub fn new(texture_creator: &'tm TextureCreator<WindowContext>) -> TextureManager<'tm> {
         TextureManager {
             texture_creator: texture_creator,
             items: HashMap::new(),
         }
     }
 
-    pub fn load(&'tm mut self, id: String, path: &Path) {
+    pub fn load(&mut self, id: String, path: &Path) {
         let texture = self.texture_creator.load_texture(path).unwrap();
         let wabbit = Texture::new(texture);
         self.items.insert(id, Rc::new(wabbit));
